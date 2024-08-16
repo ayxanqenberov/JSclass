0ca2ad9c35f59a86fd03a1bc243b7b0b2dabc496
098100ayxan <ganbarov008@gmail.com>
2024-08-16 21:41:50 +0400

    done

---
+++ class.js
@@ -0,0 +1,21 @@
+// Person classi yaradib ona name age ve surname verin daha sonra 
+// onun doğum ilini hesablayan class icinde bir function yazin
+// Meselen age 20 verdiyimizde function 2004 nəticəsini versin
+// Əlavə olaraq OOP ilə əlaqəli məqalələr atacam onlarada mütləq nəzər yetirin
+
+// class Person {
+//     constructor(name,age,surname){
+//         this.name = name;
+//         this.age = age;
+//         this.surname = surname;
+//     }
+//     birth_year(){
+//         let year = 2024;
+//         let birthYearFind = undefined;
+//         return birthYearFind = year - this.age
+//     }
+// }
+// const person = new Person("John",19,"Steve");
+// console.log(person,JSON.stringify(person.birth_year()));
+
+
